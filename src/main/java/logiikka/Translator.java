5b5976e352be4bd32381e7b4c1dc65071a2e6403
emivo <emil.airta@helsinki.fi>
2016-04-12 17:51:25 +0300

    Update Translator

--- src/main/java/logiikka/Translator.java
+++ src/main/java/logiikka/Translator.java
@@ -26,28 +26,41 @@ public class Translator {
 
     private void appendField(StringBuilder sb, String field, Book book) {
         sb.append("\t");
-        sb.append(field);
+        sb.append(compileUmlauts(field));
         sb.append(" = ");
         inputParam(sb, book.getField(field));
     }
-    
-    public static String compileUmlauts(String s) {
-        String compiled = s.toLowerCase();
-        compiled = compiled.toUpperCase().charAt(0) + compiled.substring(1);
-        compiled = compiled.replace("ä", "\\\"{a}")
-                           .replace("ö", "\\\"{o}")
-                           .replace("å", "\\aa");
 
-        return compiled;
+    public String compileUmlauts(String s) {
+        String ret = capsuleUpperCases(s);
+        ret = ret.replace("ä", "\\\"{a}")
+                .replace("ö", "\\\"{o}")
+                .replace("å", "\\aa");
+
+        return ret;
     }
-    
+
+    private String capsuleUpperCases(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                sb.append("{");
+                sb.append(s.charAt(i));
+                sb.append("{");
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
     private void inputParam(StringBuilder sb, String field) {
         sb.append("\"");
         String compiledfield = compileUmlauts(field);
         sb.append(compiledfield);
         sb.append("\",\n");
     }
-    
+
     private void writeInFile(String bibtexString) {
         try {
             fw.write(bibtexString);
