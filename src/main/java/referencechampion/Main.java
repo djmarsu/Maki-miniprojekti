0bca5237f2a052e5ddf1eb2ff92a7c12bd64696b
n <n@n.n>
2016-04-12 16:16:01 +0300

    siistitty

--- src/main/java/referencechampion/Main.java
+++ src/main/java/referencechampion/Main.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package referencechampion;
 
 import gui.Kayttoliittyma;
 import javax.swing.SwingUtilities;
 
-/**
- *
- * @author juhokyyh
- */
 public class Main {
-    public static void main(String[] args) {    
+    public static void main(String[] args) {
         Kayttoliittyma kl = new Kayttoliittyma(600, 600);
         SwingUtilities.invokeLater(kl);
     }
