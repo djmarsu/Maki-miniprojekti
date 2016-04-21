d62c5c60fccc0051f9229ddbf4cbe0c1dab40e80
nn <nn@nn.n>
2016-04-21 16:45:56 +0300

    noita kai ei käytetä enää

--- src/main/java/referencechampion/ReferenceValidator.java
+++ src/main/java/referencechampion/ReferenceValidator.java
@@ -1,9 +1,6 @@
-
-
 package referencechampion;
 
 import java.util.List;
-
 
 public class ReferenceValidator {
     public boolean validate(Reference ref) {
@@ -12,44 +9,10 @@ public class ReferenceValidator {
         for (String req : requirements) {
             if (!validateField(ref.getField(req))) return false;
         }
-        return true;
-        
-        
+        return true;       
     }
     
     private boolean validateField(String field) {
         return field!=null && !field.equals("");
-    }
-    
-    // ei käytössä ##################################
-    private boolean validateAll(ReferenceEntity s) {
-        ReferenceCollection refCol = new ReferenceCollection();
-        
-        List<String> requirements = refCol.getReferenceRequirements(s.getType());
-        for (String requirement : requirements) {
-            if (!validateField(s.getField(requirement))) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    private boolean validateBook(ReferenceEntity book) {
-        List<String> requirements = ReferenceCollection.getReferenceRequirements("book");
-        
-        for (String req : requirements) {
-            if (!validateField(req)) return false;
-        }
-        return validateField(book.getField("publisher")) && validateField(book.getField("title")) && validateField(book.getField("year"));
-    }
-
-    private boolean validateInproceedings(ReferenceEntity inproceedings) {
-        return validateField(inproceedings.getField("author")) && validateField(inproceedings.getField("booktitle")) && validateField(inproceedings.getField("title")) && validateField(inproceedings.getField("year"));
-    }
-
-    private boolean validateArticle(ReferenceEntity article) {
-        return validateField(article.getField("author")) && validateField(article.getField("journal")) && validateField(article.getField("title")) && validateField(article.getField("year"));
-    }
-    
+    }    
 }
