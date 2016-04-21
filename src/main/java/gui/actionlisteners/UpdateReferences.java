d528ff070539269ac07945591fc5c2bdd37c9b3e
nn <nn@nn.n>
2016-04-21 16:23:17 +0300

    muka tommone etsintä kenttä :D

--- src/main/java/gui/actionlisteners/UpdateReferences.java
+++ src/main/java/gui/actionlisteners/UpdateReferences.java
@@ -1,35 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui.actionlisteners;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import referencechampion.Reference;
 import referencechampion.ReferenceBase;
 
-/**
- *
- * @author emivo
- */
 public class UpdateReferences implements ActionListener {
     ReferenceBase base;
     JTextArea listingArea;
+    JTextField filterField;
 
-    public UpdateReferences(ReferenceBase base, JTextArea listingArea) {
+    public UpdateReferences(ReferenceBase base, JTextArea listingArea, JTextField filter) {
         this.base = base;
         this.listingArea = listingArea;
+        this.filterField = filter;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        String filter = filterField.getText();
+        
         StringBuilder sb = new StringBuilder();
         for (Reference reference : base.getReferences()) {
-            sb.append(reference.toString());
-            sb.append("\n");
+            boolean toAdd = false;
+            if (filter.isEmpty()) {
+                toAdd = true;
+            } else {
+                List<String> fields = reference.getFields();
+                for (String f : fields) {
+                    if (reference.getField(f).contains(filter)) {
+                        toAdd = true;
+                    }
+                }
+            }
+            if (toAdd) {
+                sb.append(reference.toString());
+                sb.append("\n");
+            }
+
         }
         listingArea.setText(sb.toString());
     }
