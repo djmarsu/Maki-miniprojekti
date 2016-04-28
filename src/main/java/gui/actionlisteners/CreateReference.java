9822dc381089762147330a9c770bfadd741d85a7
n <n@n.n>
2016-04-28 14:09:17 +0300

    tag thing added and timerized 5 second New Reference Added text

--- src/main/java/gui/actionlisteners/CreateReference.java
+++ src/main/java/gui/actionlisteners/CreateReference.java
@@ -7,6 +7,8 @@ package gui.actionlisteners;
 
 import gui.Field;
 import gui.FieldCreator;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -14,6 +16,7 @@ import java.util.Map;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.Timer;
 import referencechampion.ReferenceBase;
 import referencechampion.ReferenceEntity;
 
@@ -43,11 +46,22 @@ public class CreateReference implements ActionListener {
         if (fields != null) {
             for (String s : fields.keySet()) {
                 reference.addValue(s, fields.get(s).getText());
-            }          
+            }
         }
 
         if (base.addReference(reference)) {
+            result.setForeground(Color.green);
             result.setText("New reference added");
+            // result.setFont(new Font(result.getName(), Font.PLAIN, 22));
+
+            ActionListener taskPerformer = new ActionListener() {
+                public void actionPerformed(ActionEvent evt) {
+                    result.setText("");
+                }
+            };
+            Timer timer = new Timer(5000, taskPerformer);
+            timer.setRepeats(false);
+            timer.start();
             FieldCreator.emptyFields(this.fields);
         } else {
             result.setText("One or more required fields are empty");
